d9d3a308475a42ddb891ffdabcca24832a3acc23
DanielSitumeang7 <dsitumeang7@gmail.com>
2025-09-28 01:49:15 +0700

    error handling jwt

--- src/auth/guards/jwt/jwt.guard.ts
+++ src/auth/guards/jwt/jwt.guard.ts
@@ -1,6 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtGuard extends AuthGuard("jwt") {
+    handleRequest<TUser = any>(
+        err: Error | null,
+        user: TUser | false,
+        info: { message?: string } | undefined,
+        context: ExecutionContext,
+        status?: number,
+    ): TUser {
+        if (err || !user) {
+            const message =
+                info?.message || 'Token tidak valid atau sudah kedaluwarsa';
+            throw new UnauthorizedException(message);
+        }
+        return user;
+    }
 }
